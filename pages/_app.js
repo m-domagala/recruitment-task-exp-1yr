ba169c277e3115d86d2fc97e59b7d361bcc3349a
Michal Domagala <michaldomagala@hotmail.com>
2022-11-08 13:24:04 +0100

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import { useState } from 'react';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
 import { CartContext } from '../context/context';
 
 import Layout from '../components/layout/layout';
 
 import '../styles/index.scss';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
  const [cartState, setCartState] = useState([]);
